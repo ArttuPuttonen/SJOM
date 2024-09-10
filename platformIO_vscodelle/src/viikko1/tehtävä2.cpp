d8f089f9f979ec25bb1b6158b34a226615d46dab
Jaakko Simonen <jaakko.simonen@gmail.com>
2024-09-10 12:04:24 +0300

    T3

--- platformIO_vscodelle/src/viikko1/tehtävä2.cpp
+++ platformIO_vscodelle/src/viikko1/tehtävä2.cpp
@@ -1,55 +1,55 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// Määritellään LEDien pin numerot
-const int redLED = 11;
-const int yellowLED = 10;
-const int greenLED = 9;
+// // Määritellään LEDien pin numerot
+// const int redLED = 11;
+// const int yellowLED = 10;
+// const int greenLED = 9;
 
-// Taajuus 2 Hz tarkoittaa vilkkumisjaksoa 0.5 sekuntia (500 ms)
-const int blinkInterval = 500;
+// // Taajuus 2 Hz tarkoittaa vilkkumisjaksoa 0.5 sekuntia (500 ms)
+// const int blinkInterval = 500;
 
-void setup() {
-  // Asetetaan LED pinnit ulostuloiksi
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-}
+// void setup() {
+//   // Asetetaan LED pinnit ulostuloiksi
+//   pinMode(redLED, OUTPUT);
+//   pinMode(yellowLED, OUTPUT);
+//   pinMode(greenLED, OUTPUT);
+// }
 
-// Funktio LEDien sytyttämiseen ja sammuttamiseen
-void turnOnRed() {
-  digitalWrite(redLED, LOW);
-}
+// // Funktio LEDien sytyttämiseen ja sammuttamiseen
+// void turnOnRed() {
+//   digitalWrite(redLED, LOW);
+// }
 
-void turnOffRed() {
-  digitalWrite(redLED, HIGH);
-}
+// void turnOffRed() {
+//   digitalWrite(redLED, HIGH);
+// }
 
-void turnOnYellow() {
-  digitalWrite(yellowLED, HIGH);
-}
+// void turnOnYellow() {
+//   digitalWrite(yellowLED, HIGH);
+// }
 
-void turnOffYellow() {
-  digitalWrite(yellowLED, LOW);
-}
+// void turnOffYellow() {
+//   digitalWrite(yellowLED, LOW);
+// }
 
-void turnOnGreen() {
-  digitalWrite(greenLED, HIGH);
-}
+// void turnOnGreen() {
+//   digitalWrite(greenLED, HIGH);
+// }
 
-void turnOffGreen() {
-  digitalWrite(greenLED, LOW);
-}
+// void turnOffGreen() {
+//   digitalWrite(greenLED, LOW);
+// }
 
-void loop() {
-  // Sytytetään kaikki kolme LEDiä
-  turnOnRed();
-  turnOnYellow();
-  turnOnGreen();
-  delay(blinkInterval);  // Odotetaan 500 ms
+// void loop() {
+//   // Sytytetään kaikki kolme LEDiä
+//   turnOnRed();
+//   turnOnYellow();
+//   turnOnGreen();
+//   delay(blinkInterval);  // Odotetaan 500 ms
   
-  // Sammutetaan kaikki kolme LEDiä
-  turnOffRed();
-  turnOffYellow();
-  turnOffGreen();
-  delay(blinkInterval);  // Odotetaan 500 ms
-}
+//   // Sammutetaan kaikki kolme LEDiä
+//   turnOffRed();
+//   turnOffYellow();
+//   turnOffGreen();
+//   delay(blinkInterval);  // Odotetaan 500 ms
+// }

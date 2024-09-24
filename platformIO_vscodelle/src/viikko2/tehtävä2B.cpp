44108d58bd04af0fd8f0c53e07e4a26ba0e316d8
Arttu Puttonen <arskaputtonen@gmail.com>
2024-09-24 09:01:46 +0300

    teht3A

--- platformIO_vscodelle/src/viikko2/tehtävä2B.cpp
+++ platformIO_vscodelle/src/viikko2/tehtävä2B.cpp
@@ -1,41 +1,41 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-#define RED_LED    PB3
-#define YELLOW_LED PB2
-#define GREEN_LED  PB1
+// #define RED_LED    PB3
+// #define YELLOW_LED PB2
+// #define GREEN_LED  PB1
 
-void setup() {
-    DDRB |= (1 << RED_LED) | (1 << YELLOW_LED) | (1 << GREEN_LED);
+// void setup() {
+//     DDRB |= (1 << RED_LED) | (1 << YELLOW_LED) | (1 << GREEN_LED);
 
-    PORTB |= (1 << GREEN_LED);
-    delay(1000);
-    PORTB &= ~(1 << GREEN_LED);
+//     PORTB |= (1 << GREEN_LED);
+//     delay(1000);
+//     PORTB &= ~(1 << GREEN_LED);
     
-    PORTB |= (1 << YELLOW_LED);
-    delay(1000);
-    PORTB &= ~(1 << YELLOW_LED);
+//     PORTB |= (1 << YELLOW_LED);
+//     delay(1000);
+//     PORTB &= ~(1 << YELLOW_LED);
     
-    PORTB |= (1 << RED_LED);
-    delay(1000);
-    PORTB &= ~(1 << RED_LED);
-}
+//     PORTB |= (1 << RED_LED);
+//     delay(1000);
+//     PORTB &= ~(1 << RED_LED);
+// }
 
-void loop() {
-    PORTB |= (1 << GREEN_LED);
-    PORTB &= ~(1 << YELLOW_LED);
-    PORTB &= ~(1 << RED_LED);
-    delay(3000);
+// void loop() {
+//     PORTB |= (1 << GREEN_LED);
+//     PORTB &= ~(1 << YELLOW_LED);
+//     PORTB &= ~(1 << RED_LED);
+//     delay(3000);
 
-    PORTB &= ~(1 << GREEN_LED);
-    PORTB |= (1 << YELLOW_LED);
-    delay(1000);
+//     PORTB &= ~(1 << GREEN_LED);
+//     PORTB |= (1 << YELLOW_LED);
+//     delay(1000);
 
-    PORTB &= ~(1 << YELLOW_LED);
-    PORTB |= (1 << RED_LED);
-    delay(3000);
+//     PORTB &= ~(1 << YELLOW_LED);
+//     PORTB |= (1 << RED_LED);
+//     delay(3000);
 
-    PORTB |= (1 << YELLOW_LED);
-    delay(1000);
+//     PORTB |= (1 << YELLOW_LED);
+//     delay(1000);
 
-    PORTB &= ~(1 << YELLOW_LED);
-}
+//     PORTB &= ~(1 << YELLOW_LED);
+// }

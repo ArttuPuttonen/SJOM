4ab80ca394afd179f5e78be1761811ecb9e08e06
Jaakko Simonen <jaakko.simonen@gmail.com>
2024-09-24 11:14:59 +0300

    viikon 3 tehtävät 1,2,3

--- platformIO_vscodelle/src/viikko3/tehtävä1A.cpp
+++ platformIO_vscodelle/src/viikko3/tehtävä1A.cpp
@@ -1,43 +1,43 @@
-#include <Arduino.h>
-#include <TimerOne.h>  // TimerOne-kirjasto
+// #include <Arduino.h>
+// #include <TimerOne.h>  // TimerOne-kirjasto
 
-// Määritellään LEDien pin numerot
-const int redLED = 11;
-const int yellowLED = 10;
-const int greenLED = 9;
+// // Määritellään LEDien pin numerot
+// const int redLED = 11;
+// const int yellowLED = 10;
+// const int greenLED = 9;
 
-bool ledState = false;  // Muuttuja LEDien tilan seuraamiseen
+// bool ledState = false;  // Muuttuja LEDien tilan seuraamiseen
 
-void toggleLEDs() {
-  // Vaihdetaan LEDien tila
-  ledState = !ledState;
+// void toggleLEDs() {
+//   // Vaihdetaan LEDien tila
+//   ledState = !ledState;
   
-  if (ledState) {
-    // Sytytetään LEDit
-    digitalWrite(redLED, LOW);
-    digitalWrite(yellowLED, HIGH);
-    digitalWrite(greenLED, HIGH);
-  } else {
-    // Sammutetaan LEDit
-    digitalWrite(redLED, HIGH);
-    digitalWrite(yellowLED, LOW);
-    digitalWrite(greenLED, LOW);
-  }
-}
+//   if (ledState) {
+//     // Sytytetään LEDit
+//     digitalWrite(redLED, HIGH);
+//     digitalWrite(yellowLED, HIGH);
+//     digitalWrite(greenLED, HIGH);
+//   } else {
+//     // Sammutetaan LEDit
+//     digitalWrite(redLED, LOW);
+//     digitalWrite(yellowLED, LOW);
+//     digitalWrite(greenLED, LOW);
+//   }
+// }
 
-void setup() {
-  // Asetetaan LED pinnit ulostuloiksi
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
+// void setup() {
+//   // Asetetaan LED pinnit ulostuloiksi
+//   pinMode(redLED, OUTPUT);
+//   pinMode(yellowLED, OUTPUT);
+//   pinMode(greenLED, OUTPUT);
   
-  // Asetetaan ajastin keskeyttämään joka 500 ms (0.5 sekuntia)
-  Timer1.initialize(500000);  // 500000 mikrosekuntia = 500 ms
-  Timer1.attachInterrupt(toggleLEDs);  // Liitetään keskeytys funktioon
-}
+//   // Asetetaan ajastin keskeyttämään joka 500 ms (0.5 sekuntia)
+//   Timer1.initialize(500000);  // 500000 mikrosekuntia = 500 ms
+//   Timer1.attachInterrupt(toggleLEDs);  // Liitetään keskeytys funktioon
+// }
 
 
 
-void loop() {
-  // Ei tarvita mitään koodia loop-funktiossa, koska ajastinkeskeytys hoitaa vilkutuksen
-}
+// void loop() {
+//   // Ei tarvita mitään koodia loop-funktiossa, koska ajastinkeskeytys hoitaa vilkutuksen
+// }
---
+++ platformIO_vscodelle/src/viikko3/tehtävä3v2.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+
+// Määritellään LEDien pin numerot
+const int redLED = 11;    // Punainen valo
+const int yellowLED = 10; // Keltainen valo
+const int greenLED = 9;   // Vihreä valo
+
+// Määritellään napin pin
+const int buttonPin = 2;  // Nappi kytketty pinniin 2
+
+volatile bool yellowBlinking = false;  // Muuttuja keltaisen valon vilkkumiselle
+volatile bool normalSequenceActive = true;  // Muuttuja normaalin sekvenssin hallinnalle
+
+void toggleYellowBlink() {
+  yellowBlinking = !yellowBlinking;  // Vaihda keltaisen vilkkumisen tila
+  normalSequenceActive = !yellowBlinking;  // Normaalin sekvenssin tila käänteinen keltaisen vilkkumiselle
+}
+
+void blinkYellow() {
+  // Jos keltaisen vilkkuminen on aktiivinen, vilkuta keltainen valo
+  if (yellowBlinking) {
+    digitalWrite(yellowLED, LOW);   // Sytytä keltainen valo (LOW)
+    delay(500);                      // Odota 500 ms
+    digitalWrite(yellowLED, HIGH);  // Sammuta keltainen valo (HIGH)
+    delay(500);                      // Odota 500 ms
+  }
+}
+
+void startTrafficSequence() {
+  // Normaalin liikennevalosekvenssin suoritus
+  if (normalSequenceActive) {
+    // Vihreä valo palaa 3 sekuntia
+    digitalWrite(greenLED, HIGH);  // Sytytä vihreä valo (HIGH)
+    delay(3000);
+    digitalWrite(greenLED, LOW);   // Sammuta vihreä valo (LOW)
+
+    // Keltainen valo palaa sekunnin
+    digitalWrite(yellowLED, HIGH);  // Sytytä keltainen valo (HIGH)
+    delay(1000);
+    digitalWrite(yellowLED, LOW);   // Sammuta keltainen valo (LOW)
+
+    // Punainen valo palaa 3 sekuntia
+    digitalWrite(redLED, HIGH);     // Sytytä punainen valo (HIGH)
+    delay(3000);
+    digitalWrite(redLED, LOW);      // Sammuta punainen valo (LOW)
+
+    // Punainen ja keltainen yhdessä sekunnin ajan
+    digitalWrite(redLED, HIGH);     // Sytytä punainen valo (HIGH)
+    digitalWrite(yellowLED, HIGH);  // Sytytä keltainen valo (HIGH)
+    delay(1000);
+    digitalWrite(redLED, LOW);      // Sammuta punainen valo (LOW)
+    digitalWrite(yellowLED, LOW);   // Sammuta keltainen valo (LOW)
+  }
+}
+
+void setup() {
+  // Asetetaan LED pinnit ulostuloiksi
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+
+  // Asetetaan nappi sisääntuloksi
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Asetetaan keskeytys napin painallukseen (FALLING-moodi)
+  attachInterrupt(digitalPinToInterrupt(buttonPin), toggleYellowBlink, FALLING);
+
+  // Sarjamonitorin käynnistys (valinnainen)
+  Serial.begin(9600);
+
+  // Aluksi sammutetaan kaikki valot
+  digitalWrite(redLED, LOW);      // Sammuta punainen valo (LOW)
+  digitalWrite(yellowLED, LOW);   // Sammuta keltainen valo (LOW)
+  digitalWrite(greenLED, LOW);    // Sammuta vihreä valo (LOW)
+}
+
+void loop() {
+  // Suoritetaan liikennevalosekvenssi ja vilkkuva keltainen valo
+  startTrafficSequence();
+  blinkYellow();
+}

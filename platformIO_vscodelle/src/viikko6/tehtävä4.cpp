422a72f5eb9113c6708616470adc452beff65b54
Jaakko Simonen <jaakko.simonen@gmail.com>
2024-11-05 09:37:56 +0200

    .ini-tiedostoon päivitetty kirjastoja. Viikko8 t1 eka versio

--- platformIO_vscodelle/src/viikko6/tehtävä4.cpp
+++ platformIO_vscodelle/src/viikko6/tehtävä4.cpp
@@ -1,27 +1,27 @@
-#include <Arduino.h>
-#include <avr/wdt.h>
+// #include <Arduino.h>
+// #include <avr/wdt.h>
 
-const int ledPin = 9; // LED-pinni
-const int watchdogTimeout = 4; // Watchdog timeout (4 sekuntia)
+// const int ledPin = 9; // LED-pinni
+// const int watchdogTimeout = 4; // Watchdog timeout (4 sekuntia)
 
-void setup() {
-  Serial.begin(9600); // Aloitetaan sarjayhteys
-  Serial.println("Ollaan setupissa"); // Tulostetaan viesti
+// void setup() {
+//   Serial.begin(9600); // Aloitetaan sarjayhteys
+//   Serial.println("Ollaan setupissa"); // Tulostetaan viesti
 
-  pinMode(ledPin, OUTPUT); // Asetetaan LED-pinni ulostuloksi
-  wdt_enable(WDTO_4S); // Otetaan watchdog käyttöön 4 sekunnin aikarajalla
-}
+//   pinMode(ledPin, OUTPUT); // Asetetaan LED-pinni ulostuloksi
+//   wdt_enable(WDTO_4S); // Otetaan watchdog käyttöön 4 sekunnin aikarajalla
+// }
 
-void loop() {
-  for (int i = 0; i < 5; i++) { // 5 kierrosta
-    digitalWrite(ledPin, HIGH); // Sytytetään LED
-    delay(1000); // Odotetaan 1000 ms
-    wdt_reset(); // Nollataan watchdog timer
+// void loop() {
+//   for (int i = 0; i < 5; i++) { // 5 kierrosta
+//     digitalWrite(ledPin, HIGH); // Sytytetään LED
+//     delay(1000); // Odotetaan 1000 ms
+//     wdt_reset(); // Nollataan watchdog timer
 
-    digitalWrite(ledPin, LOW); // Sammutetaan LED
-    delay(1000); // Odotetaan 1000 ms
-    wdt_reset(); // Nollataan watchdog timer
-  }
+//     digitalWrite(ledPin, LOW); // Sammutetaan LED
+//     delay(1000); // Odotetaan 1000 ms
+//     wdt_reset(); // Nollataan watchdog timer
+//   }
 
-  // Voidaan lisätä muita toimintoja tai logiikkaa tässä
-}
+//   // Voidaan lisätä muita toimintoja tai logiikkaa tässä
+// }

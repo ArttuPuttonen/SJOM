ebafefd02b95ef066101ee16950aaa04973573f4
Arttu Puttonen <arskaputtonen@gmail.com>
2024-11-05 09:32:07 +0200

    pohja lisätty harkkatyölle

---
+++ bitcoin_tracker/src/main.cpp
@@ -0,0 +1,31 @@
+
+// #include <ESP8266WiFi.h>
+// #include <ESP8266HTTPClient.h>
+
+// // WiFi-asetukset
+// const char* ssid = "wifi name here";       
+// const char* password = "Password here";
+
+
+// void setup() {
+//   Serial.begin(115200);  // Avaa sarjayhteys kommunikointia varten
+
+//   // Yhdistä WiFi-verkkoon
+//   WiFi.begin(ssid, password);
+//   Serial.print("Connecting to WiFi");
+
+//   // Odota, että yhteys muodostuu
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(1000);
+//     Serial.print(".");
+//   }
+  
+//   Serial.println("");
+//   Serial.println("Connected to WiFi");
+//   Serial.print("IP Address: ");
+//   Serial.println(WiFi.localIP());
+// }
+
+// void loop() {
+//   // Voit lisätä tähän muun logiikan tai tehdä HTTP-pyyntöjä
+// }

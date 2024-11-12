ea3814180c686afd7f814f90a400c89b26b87189
Arttu Puttonen <arskaputtonen@gmail.com>
2024-11-12 09:40:30 +0200

    bitcoin tracker toimii serialissa

--- bitcoin_tracker/src/testi.cpp
+++ bitcoin_tracker/src/testi.cpp
@@ -1,13 +1,13 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup() {
-  // Avaa sarjayhteys nopeudella 115200 baud
-  Serial.begin(115200);
-  Serial.println("Hello from ESP8266!");  // Tulostaa viestin sarjamonitoriin
-}
+// void setup() {
+//   // Avaa sarjayhteys nopeudella 115200 baud
+//   Serial.begin(115200);
+//   Serial.println("Hello from ESP8266!");  // Tulostaa viestin sarjamonitoriin
+// }
 
-void loop() {
-  // Tulostaa viestin sarjamonitoriin joka sekunti
-  Serial.println("Looping...");
-  delay(1000);  // Odottaa 1000 millisekuntia eli 1 sekunnin
-}
+// void loop() {
+//   // Tulostaa viestin sarjamonitoriin joka sekunti
+//   Serial.println("Looping...");
+//   delay(1000);  // Odottaa 1000 millisekuntia eli 1 sekunnin
+// }
